3f0e05a010eaeab976188dfc89d6306768c0a8a0
Unai Iñigo Insausti <unainigo1@gmail.com>
2021-03-31 19:19:00 +0200

    Primer programa a subir

---
+++ prueba.py
@@ -0,0 +1,7 @@
+for i in ["unai", "pablito", "olga", "charlie", "mary", "fermin", "navarro"]:
+	
+	if i == "olga" or i == "mary":
+		print(i + " es una chica de la cuadrilla")
+	else:
+		print(i + " no es una chica de la cuadrilla")
+print("el programa simple de prueba acaba aqui")
